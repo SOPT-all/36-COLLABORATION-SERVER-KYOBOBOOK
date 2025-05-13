038818711b9d1c1bc25d5b77bdfcc9492b86bf82
dltnals317 <sumin4101@naver.com>
2025-05-14 04:04:43 +0900

    [FIX] yml수정

--- src/main/java/org/sopt/controller/book/BookController.java
+++ src/main/java/org/sopt/controller/book/BookController.java
@@ -1,21 +1,24 @@
 package org.sopt.controller.book;
 
+import lombok.RequiredArgsConstructor;
 import org.sopt.service.book.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class BookController {
+
+    @Autowired
     private final BookService bookService;
 
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
+
     @GetMapping("/health")
-    public ResponseEntity<String> health() {
+    public ResponseEntity<String> test() {
         return ResponseEntity.ok("OK");
     }
 }
