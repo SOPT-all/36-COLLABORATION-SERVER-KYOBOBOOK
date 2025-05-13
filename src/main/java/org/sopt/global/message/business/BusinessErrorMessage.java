85621783038df0e14246764ce00de22fb6a904df
leejisoo <leejisoo0617@naver.com>
2025-05-13 14:36:43 +0900

    [FEAT] 프로젝트구조설정 완료

--- src/main/java/org/sopt/global/message/business/BusinessErrorMessage.java
+++ src/main/java/org/sopt/global/message/business/BusinessErrorMessage.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum BusinessErrorMessage implements DefaultErrorMessage{
+public enum BusinessErrorMessage implements DefaultErrorMessage {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     MISSING_REQUIRED_HEADER(HttpStatus.BAD_REQUEST, "필수 헤더가 누락되었습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
