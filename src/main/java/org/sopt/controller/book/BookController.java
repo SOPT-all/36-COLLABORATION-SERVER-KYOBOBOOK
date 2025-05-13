bbba0ccee45e5a3da9e52365f24c8d627b5b5734
dltnals317 <sumin4101@naver.com>
2025-05-14 03:22:35 +0900

    [TEST]Controller test

--- src/main/java/org/sopt/controller/book/BookController.java
+++ src/main/java/org/sopt/controller/book/BookController.java
@@ -3,9 +3,11 @@ package org.sopt.controller.book;
 import org.sopt.service.book.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class BookController {
     private final BookService bookService;
 
