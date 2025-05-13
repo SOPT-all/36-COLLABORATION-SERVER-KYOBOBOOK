c97ec2eabd882c7c24b67e70edf836f369c52783
dltnals317 <sumin4101@naver.com>
2025-05-14 02:48:34 +0900

    [FIX] yml수정-DB_HOST변경

--- src/main/java/org/sopt/controller/book/BookController.java
+++ src/main/java/org/sopt/controller/book/BookController.java
@@ -1,6 +1,8 @@
 package org.sopt.controller.book;
 
 import org.sopt.service.book.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,9 @@ public class BookController {
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
     }
 }
