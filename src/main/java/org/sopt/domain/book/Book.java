2a840f7d03eb0301dd82f8c6e81efeceea71e3cc
dltnals317 <sumin4101@naver.com>
2025-05-15 18:59:38 +0900

    [FEAT] 엔티티 수정사항 반영,설계

--- src/main/java/org/sopt/domain/book/Book.java
+++ src/main/java/org/sopt/domain/book/Book.java
@@ -1,27 +1,37 @@
 package org.sopt.domain.book;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.sopt.domain.review.Review;
+import org.sopt.domain.review.statistic.ReviewStatistic;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "book")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long bookId;
     private String title;
     private String price;
     private String publisher;
     private String bookIndex;
     private String description;
-    private LocalDate date;
+    private LocalDate publishDate;
     private String author;
-    private Long ranking;
+    private Long rank;
+
+
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private ReviewStatistic reviewStatistic;
 
     @OneToMany(mappedBy = "book")
-    private List<Review> reviews;
+    private List<Review> reviews =new ArrayList<>();
 }
